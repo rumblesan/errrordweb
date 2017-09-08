507416ca0a2d98f2dd27ec62b8d30311a44bae3a
Guy John <guy@rumblesan.com>
2017-09-08 09:39:21 +0100

    Move App into container
    delete test untill I can bother mocking sub containers

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 
 import 'index.css';
 
-import App from 'containers/App';
+import App from 'containers/App/container';
 
 import configureStore from 'store/configureStore';
 
