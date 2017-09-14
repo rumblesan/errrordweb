0b7dd9d3996ead691578ff8f42e217a343b82146
Guy John <guy@rumblesan.com>
2017-09-14 22:27:01 +0100

    Improve load image button styling

--- src/containers/LoadSaveWidget/index.js
+++ src/containers/LoadSaveWidget/index.js
@@ -4,17 +4,20 @@ export default function LoadSaveWidget({ loadImage, saveImage }) {
   return (
     <div className="loadsave-widget">
       <h3>Load/Save</h3>
-      <input
-        onChange={evt => {
-          const files = evt.target.files;
-          if (files[0]) {
-            loadImage(files[0]);
-          }
-        }}
-        type="file"
-        id="files"
-        name="files[]"
-      />
+      <label className="file-input pure-button">
+        <input
+          onChange={evt => {
+            const files = evt.target.files;
+            if (files[0]) {
+              loadImage(files[0]);
+            }
+          }}
+          type="file"
+          id="files"
+          name="files[]"
+        />
+        <span>Load Image</span>
+      </label>
       <button className="pure-button" onClick={saveImage}>
         Save Image
       </button>
