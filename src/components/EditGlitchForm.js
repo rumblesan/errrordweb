d23e33863df6164231782c4f8745b2e68d4e34df
Guy John <guy@rumblesan.com>
2017-09-14 22:09:22 +0100

    Move EditGlitch form into its own component

---
+++ src/components/EditGlitchForm.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+
+import { quantizeGlitch } from 'glitch/types';
+
+class EditGlitchForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      type: 'quantize',
+      depth: 0.05,
+      seed: Math.random(),
+      quantTable: 'all',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(key) {
+    return event => {
+      this.setState({ [key]: event.target.value });
+    };
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const { seed, depth, quantTable } = this.state;
+    this.props.createNewGlitch(quantizeGlitch(seed, depth, quantTable));
+  }
+
+  render() {
+    return (
+      <form
+        className="pure-form pure-form-aligned"
+        onSubmit={this.handleSubmit}
+      >
+        <fieldset>
+          <div className="pure-control-group">
+            <label htmlFor="edit-glitch-seed">Seed:</label>
+            <input
+              id="edit-glitch-seed"
+              type="text"
+              value={this.state.seed}
+              onChange={this.handleChange('seed')}
+            />
+          </div>
+
+          <div className="pure-control-group">
+            <label htmlFor="edit-glitch-depth">Depth:</label>
+            <input
+              id="edit-glitch-depth"
+              type="text"
+              value={this.state.depth}
+              onChange={this.handleChange('depth')}
+            />
+          </div>
+          <div className="pure-control-group">
+            <label htmlFor="edit-glitch-quant">Quant Table:</label>
+            <input
+              id="edit-glitch-quant"
+              type="text"
+              value={this.state.quantTable}
+              onChange={this.handleChange('quantTable')}
+            />
+          </div>
+          <div className="pure-controls">
+            <button type="submit" className="pure-button pure-button-primary">
+              Create
+            </button>
+            <button className="pure-button" onClick={this.props.cancelCreate}>
+              Cancel
+            </button>
+          </div>
+        </fieldset>
+      </form>
+    );
+  }
+}
+
+export default EditGlitchForm;
--- src/containers/GlitchWidget/index.js
+++ src/containers/GlitchWidget/index.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import HeadedModal from 'components/EditGlitchModal';
+import EditGlitchForm from 'components/EditGlitchForm';
 import { GLITCH_EDIT_MODAL } from 'actions/modals';
-
-import { quantizeGlitch } from 'glitch/types';
 
 export default function GlitchWidget({
   stack,
@@ -25,86 +24,11 @@ export default function GlitchWidget({
         closeModal={closeEditGlitchModal}
         title="Create Glitch"
       >
-        <GlitchCreationForm
+        <EditGlitchForm
           cancelCreate={closeEditGlitchModal}
           createNewGlitch={createNewGlitch}
         />
       </HeadedModal>
     </div>
   );
-}
-
-class GlitchCreationForm extends Component {
-  constructor() {
-    super();
-    this.state = {
-      type: 'quantize',
-      depth: 0,
-      seed: 0,
-      quantTable: 'all',
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(key) {
-    return event => {
-      this.setState({ [key]: event.target.value });
-    };
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    const { seed, depth, quantTable } = this.state;
-    this.props.createNewGlitch(quantizeGlitch(seed, depth, quantTable));
-  }
-
-  render() {
-    return (
-      <form
-        className="pure-form pure-form-aligned"
-        onSubmit={this.handleSubmit}
-      >
-        <fieldset>
-          <div className="pure-control-group">
-            <label htmlFor="edit-glitch-seed">Seed:</label>
-            <input
-              id="edit-glitch-seed"
-              type="text"
-              value={this.state.seed}
-              onChange={this.handleChange('seed')}
-            />
-          </div>
-
-          <div className="pure-control-group">
-            <label htmlFor="edit-glitch-depth">Depth:</label>
-            <input
-              id="edit-glitch-depth"
-              type="text"
-              value={this.state.depth}
-              onChange={this.handleChange('depth')}
-            />
-          </div>
-          <div className="pure-control-group">
-            <label htmlFor="edit-glitch-quant">Quant Table:</label>
-            <input
-              id="edit-glitch-quant"
-              type="text"
-              value={this.state.quantTable}
-              onChange={this.handleChange('quantTable')}
-            />
-          </div>
-          <div className="pure-controls">
-            <button type="submit" className="pure-button pure-button-primary">
-              Create
-            </button>
-            <button className="pure-button" onClick={this.props.cancelCreate}>
-              Cancel
-            </button>
-          </div>
-        </fieldset>
-      </form>
-    );
-  }
 }
